b6ce178ca1633a23846d0bdc15076fda24f2c288
IvanTrofimov052 <55573197+IvanTrofimov052@users.noreply.github.com>
2020-10-18 08:19:48 +0300

    Update drawing.py

--- drawing.py
+++ drawing.py
@@ -12,21 +12,33 @@ PINK = (230, 50, 230)
 
 pygame.init()
 
-sc = pygame.display.set_mode((max_x * 200, max_y * 200 + 50))
+sc = pygame.display.set_mode((max_x * 20, max_y * 20 + 50))
 
 
 class FieldDraw():
     def __init__(self):
-        cells = []
+        self.cells = []
         for i in range(max_y):
-            cells.append([])
+            self.cells.append([])
             for j in range(max_x):
-                coords = (i * 20, j * 20, i * 20 + 15, j * 20 + 15)
+                coords = (i * 20, j * 20, 19, 19)
                 # coords = (0,0, 0 + 15, 0 + 15)
-                cells[i].append(pygame.draw.rect(sc, WHITE, coords))
+                self.cells[i].append(pygame.draw.rect(sc, WHITE, coords))
                 print(coords)
+    
+    def move(self, calculated_field):
+      for i in range(max_y):
+        for j in range(max_x):
+          coords = (i * 20, j * 20, 19, 19)
+          if calculated_field.field[i][j].alive == True:
+            self.cells[i][j] = pygame.draw.rect(sc, GREEN, coords)
+          else:
+            self.cells[i][j] = pygame.draw.rect(sc, YELLOW, coords)
 
 
+# making a field
+arr = init_field()
+a = Field(arr)
 
 # здесь будут рисоваться фигуры
 
@@ -34,9 +46,13 @@ pygame.display.update()
 
 field = FieldDraw()
 
+clock = pygame.time.Clock()
+
 while 1:
     pygame.display.update()
-    pygame.time.delay(1000)
+    pygame.time.delay(2000)
+    a.move()
+    field.move(a)
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
             exit()
